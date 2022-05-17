05b07c5dea92a09fe11cc4515da305183ef359f3
Danny Linhardt <daniel.linhardt@gmail.com>
2022-05-17 11:48:56 -0400

    commit tree 63f3a592c055011faa4757adbd9eff6609254158

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(someNumber) {
+    if (someNumber >= 42) {
+        return someNumber - 42;
+    }
+    else {
+        return 42 - someNumber;
+    }
+}
+function distanceFromHqInFeet(someNumber) {
+    return distanceFromHqInBlocks(someNumber) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start - destination) * 264;
+    }
+    else {
+        return (destination - start) * 264;
+    }
+    }
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) < 400) {
+        return 0;
+    }
+    else if (distanceTravelledInFeet(start, destination) < 2000) {
+        return (distanceTravelledInFeet(start, destination) - 400) * .02;
+    }
+    else if (distanceTravelledInFeet(start, destination) >= 2000 && distanceTravelledInFeet(start, destination) < 2500) {
+        return 25;
+    }
+    else if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    }
+}
